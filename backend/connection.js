675462fc254d6fe216c695d174051ff845001252
rpatel <riyapatel16075@gmail.com>
2025-07-04 12:05:00 +0530

    blog

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const connectionString = "mongodb://localhost:27017/"
+const connectionString = "mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/test"
 mongoose.connect(connectionString)
 .then(()=>console.log("db.connected"))
 .catch(()=>console.log("err"))
