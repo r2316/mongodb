bbeb866cb7a8c5993f6be0e0eced945015b1b27f
rpatel <riyapatel16075@gmail.com>
2025-07-05 10:37:14 +0530

    fghj

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-const connectionString = "mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/"
+const connectionString ="mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/test"
+// const connectionString = "mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/"
 mongoose.connect(connectionString)
 .then(()=>console.log("db.connected"))
 .catch(()=>console.log("err"))
