fda3f0320c34dbf01a54f8a699570e021e6b794d
rpatel <riyapatel16075@gmail.com>
2025-07-07 12:23:54 +0530

    asd

--- backend/index.js
+++ backend/index.js
@@ -145,13 +145,19 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const path = require('path');
-const corsOptions ={
-    origin:"*",
-    // origin:['http://localhost:3000','http://localhost:5174','https://projectfrontend-fawn.vercel.app'], 
-    // credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+// const corsOptions ={
+//     origin:"*",
+//     // origin:['http://localhost:3000','http://localhost:5174','https://projectfrontend-fawn.vercel.app'], 
+//     // credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 // app.use(cors())
 const userRouter = require("./routers/userRouter")
 const Registrationrouter = require("./routers/RegistrationRouter")
