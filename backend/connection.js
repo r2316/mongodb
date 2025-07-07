ec10f3642fc415817f02251ee0176e3cd1ee2671
rpatel <riyapatel16075@gmail.com>
2025-07-07 10:11:22 +0530

    asdfg

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const connectionString ="mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/test"
 // const connectionString = "mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/"
-mongoose.connect(connectionString)
+mongoose.connect(connectionString,{useNewUrlParser:true})
 .then(()=>console.log("db.connected"))
 .catch(()=>console.log("err"))
