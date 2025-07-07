3b81e134539aff70b70e2a484ddfe91e91931655
rpatel <riyapatel16075@gmail.com>
2025-07-07 10:37:14 +0530

    tgrfdsa

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const connectionString ="mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/test"
+const connectionString ="mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/test?retryWrites=true&w=majority"
 // const connectionString = "mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/"
 mongoose.connect(connectionString,{useNewUrlParser:true})
 .then(()=>console.log("db.connected"))
