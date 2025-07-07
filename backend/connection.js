80edc9db39c78741066537ae62172491303e0146
rpatel <riyapatel16075@gmail.com>
2025-07-07 11:35:16 +0530

    sdf

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const connectionString ="mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/test?retryWrites=true&w=majority"
-// const connectionString = "mongodb+srv://riyapatel16075:riyapatel16075@cluster0.iqybl8f.mongodb.net/"
+
+// const connectionStringLocal = "mongodb://localhost:27017/"
 mongoose.connect(connectionString,{useNewUrlParser:true})
 .then(()=>console.log("db.connected"))
 .catch(()=>console.log("err"))
