70db500a6bab112627349958ed33d5d105d195b4
rpatel <riyapatel16075@gmail.com>
2025-07-07 12:37:30 +0530

    asd

--- my-frontend-app/src/config/apidetails.js
+++ my-frontend-app/src/config/apidetails.js
@@ -1,2 +1,3 @@
 // export  const API_URL = "http://localhost:8080/"
-export const API_URL = "https://mongodb-ldju.vercel.app/"
+// export const API_URL = "https://mongodb-ldju.vercel.app/"
+export const API_URL ="https://mongodb-livid.vercel.app/"
