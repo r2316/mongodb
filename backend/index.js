32f56e99e6241fb1898f7f4867b80f24bb6760bc
rpatel <riyapatel16075@gmail.com>
2025-07-07 12:09:51 +0530

    v

--- backend/index.js
+++ backend/index.js
@@ -146,8 +146,9 @@ const app = express()
 const cors = require("cors")
 const path = require('path');
 const corsOptions ={
-    origin:['http://localhost:3000','http://localhost:5174','https://projectfrontend-fawn.vercel.app'], 
-    credentials:true,            //access-control-allow-credentials:true
+    origin:"*",
+    // origin:['http://localhost:3000','http://localhost:5174','https://projectfrontend-fawn.vercel.app'], 
+    // credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
