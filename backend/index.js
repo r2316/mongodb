f5fd95035752449b05fcaeb75bba1b4cabeedbc7
rpatel <riyapatel16075@gmail.com>
2025-07-07 12:45:04 +0530

    jhgjhgj

--- backend/index.js
+++ backend/index.js
@@ -147,6 +147,7 @@ const cors = require("cors")
 const path = require('path');
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://projectfrontend-fawn.vercel.app");
+  
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
